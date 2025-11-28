bc37d0a493f7c19f8638dd76e4deb783fb74e13a
Atomixer <imedzarour2003@gmail.com>
2025-11-28 11:31:10 +0100

    PolyImed

--- app/components/Pages/Home/Home.tsx
+++ app/components/Pages/Home/Home.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="flex grow flex-col items-center justify-center bg-white text-black">
       <h1 className="text-3xl font-semibold">
-        {t("META.TITLE", { name: "PolyLivre" })}
+        {t("META.TITLE", { name: "PolyImed" })}
       </h1>
     </div>
   );
