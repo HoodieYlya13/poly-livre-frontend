4f340f27dd84aff6d522694ed0931fb4330a24ef
Ylya <ylyamartchenko@gmail.com>
2026-01-09 12:20:55 +0100

    user actions correction

--- actions/user/user.actions.ts
+++ actions/user/user.actions.ts
@@ -7,6 +7,7 @@ import {
 } from "@/utils/cookies/cookies.server";
 import { baseServerAction } from "../base.server.actions";
 import { decodeJwt } from "jose";
+import { tryCatch } from "@/utils/tryCatch";
 import { ERROR_CODES } from "@/utils/errors";
 
 export async function updateUsernameAction(username: string) {
@@ -16,19 +17,15 @@ export async function updateUsernameAction(username: string) {
       const json = await userApi.updateUsername(username);
 
       const token = await getUserAccessToken();
-      if (!token) throw new Error(ERROR_CODES.AUTH[1]);
+      if (!token) throw new Error(ERROR_CODES.AUTH[4]);
 
-      let maxAge = 0;
+      const [payload, jwtError] = await tryCatch(async () => decodeJwt(token));
 
-      try {
-        const payload = await decodeJwt(token);
-        if (payload.exp) {
-          const now = Math.floor(Date.now() / 1000);
-          maxAge = payload.exp - now;
-        }
-      } catch {
+      if (jwtError || !payload || !payload.exp)
         throw new Error(ERROR_CODES.SYST[1]);
-      }
+
+      const now = Math.floor(Date.now() / 1000);
+      const maxAge = payload.exp - now;
 
       return await setServerCookie("user_name", json.username, {
         maxAge,
@@ -41,9 +38,9 @@ export async function updateUsernameAction(username: string) {
   );
 }
 
-export async function getUserAction() {
+export async function getCurrentUserAction() {
   return baseServerAction(
-    "getUser",
+    "getCurrentUser",
     async () => {
       return await userApi.getMe();
     },
