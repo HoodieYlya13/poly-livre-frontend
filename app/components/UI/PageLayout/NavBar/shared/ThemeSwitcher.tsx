db635828e9dda52dc4514da62fc8301f86604384
Ylya <ylyamartchenko@gmail.com>
2026-01-18 00:27:13 +0100

    resizing SVGs

--- app/components/UI/PageLayout/NavBar/shared/ThemeSwitcher.tsx
+++ app/components/UI/PageLayout/NavBar/shared/ThemeSwitcher.tsx
@@ -18,8 +18,8 @@ const renderIcon = (t: string) => (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       height="32px"
-      viewBox="0 -960 960 960"
       width="32px"
+      viewBox="30 -930 900 900"
       fill="currentColor"
     >
       <path d={getIconPath(t)} />
