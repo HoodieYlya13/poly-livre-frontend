e5c4e0f32bd3731d25290d2a448e1e263d0be17c
Ylya <ylyamartchenko@gmail.com>
2026-01-21 15:53:58 +0100

    micro fix

--- app/components/Pages/Home/shared/Testimonials/TestimonialTiles.tsx
+++ app/components/Pages/Home/shared/Testimonials/TestimonialTiles.tsx
@@ -1,11 +1,8 @@
 import TestimonialTile from "@/app/components/UI/shared/components/TestimonialTile";
-import { getPreferredLocale } from "@/utils/cookies/cookies.server";
 import { getTestimonialsAction } from "@/actions/user/private/user.private.actions";
-import { LocaleLanguages } from "@/i18n/utils";
 
 export default async function TestimonialTiles() {
-  const preferredLocale = (await getPreferredLocale()) as LocaleLanguages;
-  const testimonials = await getTestimonialsAction(preferredLocale);
+  const testimonials = await getTestimonialsAction();
 
   return (
     <div className="w-full flex flex-col xl:flex-row gap-3 items-center justify-center">
