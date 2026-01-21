a8431ba460963f565bf5242a6e3eab1bf9498b62
Ylya <ylyamartchenko@gmail.com>
2026-01-21 13:58:46 +0100

    Catalog page

--- app/components/Pages/Catalog/Catalog.tsx
+++ app/components/Pages/Catalog/Catalog.tsx
@@ -1,7 +1,27 @@
+import BookTiles from "../Home/shared/TrendingBooks/BookTiles";
+
 export default function Catalog() {
   return (
-    <div>
-      <h1>Catalog</h1>
+    <div className="flex flex-col gap-5 py-10 px-5">
+      <h1 className="text-2xl">Catalog</h1>
+
+      <div className="flex flex-col sm:flex-row gap-5 w-full">
+        <div className="flex flex-col gap-5">
+          <div>Filtres</div>
+
+          <div>Categorie</div>
+
+          <div>Prix</div>
+        </div>
+
+        <div className="flex flex-col gap-5 w-full">
+          <div className="flex ml-auto">Tri</div>
+
+          <div className="flex w-full">
+            <BookTiles className=" @4xl:grid-cols-3 @4xl:max-w-5xl" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
